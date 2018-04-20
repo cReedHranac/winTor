1e9301ff2745081ff22e2974a6e448638b988008
Carter Reed Hranac <crh244@gmail.com>
2018-04-20 14:11:42 +1200

    Database
    Added database so far (this may be the entirety of it)
    1k downscale, DB update
    
    downscale and reproject rasterlayers for speed/plotting down stream & updated 1 point from the DB as given by Cori
    More Fixes, start of prelim
    
    start main work, fixes aorund the shopw
    Models Run
    
    Ran all models, all equally trash
    Almost figured out the predictions
    
    arseholes.
    Updates for prelim modeling and raster shift
    
    Updated prelim modeling, it appears predictions are really wonky. Shifted raster items to dropbox to get cloud capabilities back up.
    Added geoMunge for proportion layers
    
    new proportion layers added for beta regression, still need to fix the old freeze and check others as well.

--- R/geoMunge.R
+++ R/geoMunge.R
@@ -1,50 +1,80 @@
 ## wintorLength geoMunge ###
-  ## Need to create some of the rasters for the testing.
+  ## Create spatial layers for project
   ## Set to North American extent
 
 ##Links
 NA.Clim <- file.path("D:", "NA_NORM_8110_Bioclim_ASCII")
 NA.Ref <- file.path("D:", "NA_Reference_files_ASCII")
 data.dir <- file.path("data")
-
+worldClim <- file.path("D:", "WorldClim", "bclim")
 ## libraries
 library(raster); library(rgdal)
 
-## October - April Layer
+## Create base layer for extent, resolution and projection (also temperature for later)
+mat <- raster(file.path(worldClim, "bio_1.bil"))
+mat.corrected <- calc(mat, function(x) x/10) #correcting the units from source
+NA.extent <- c(-172.3, -52,23.5,66.5) #extent of NA from artic circle to tropics
+mat.cropped <- crop(mat.corrected, NA.extent) #crop to NA extent
+rm(mat, mat.corrected) # clean
+
+## October - April Layer #Static layer
 NA.raster <- raster(file.path(NA.Ref, "ClimateNA_ID.asc"))
-  
-#reclassify to static value
+lcc.proj <- proj4string(NA.raster)
+  #reclassify to static value
 m <- c(-Inf,-1,NA,0,
        cellStats(NA.raster, max),182)
-NA.182 <- reclassify(NA.raster,m, 
-                     filename = file.path(data.dir, "staticWinter.tif"),
-                     format = "GTiff")
+NA.182 <- reclassify(NA.raster,m)
+names(NA.182) <- "staticWinter"
+rm(NA.raster, m)
 
 ## Number of frosty days (Inverse of the NFFD layer)
 nffd <- raster(file.path(NA.Clim, "NFFD.asc"))
-proj4string(nffd) <- proj4string(NA.raster)
-
-nfd <- calc(nffd,
-            fun = function(x) 365-x,
-            filename = file.path(data.dir, "nFrostyDays.tif"),
-            format = "GTiff")
+proj4string(nffd) <- lcc.proj
+nfd <- calc(nffd, fun = function(x) 365-x) #switch to nfrostyDays
+names(nfd) <-  "nFrostyDays"
+rm(nffd)
 
 ## Non-growing days (inverse of dd5)
 dd5 <- raster(file.path(NA.Clim, "DD5.asc"))
-
-ngd <- calc(dd5,
-            fun = function(x) 365-x,
-            filename = file.path(data.dir, "nDayBelow5.tif"),
-            format = "GTiff")
+proj4string(dd5) <- lcc.proj
+ngd <- calc(dd5, fun = function(x) 365-(x/10000)*365) #Correct from % year and get inverse
+names(ngd) <- "nonGrowingDays"
+rm(dd5)
 
 ## Lattitude raster layer
-northing <- init(NA.raster,
+northing <- init(mat.cropped,
                  fun = "y")
-northing.mask <- mask(northing, NA.182,
-                      filename = file.path(data.dir, "norhting.tif"),
-                      format = "GTiff")
+names(northing) <- "northing"
+
 ## DEM
 NA.dem <- raster(file.path(NA.Ref, "ClimateNA_DEM.asc"))
-writeRaster(NA.dem,
-            filename = file.path(data.dir, "dem.tif"),
-            format = "GTiff")
+names(NA.dem) <- "dem"
+
+## relative humidity for downstream
+rh <- raster(file.path(NA.Clim, "RH.asc"))
+proj4string(rh) <- lcc.proj
+names(rh) <- "relativeHumidity"
+
+
+## stack all together, crop and read out
+winTor <- stack(NA.182, nfd, NA.dem, rh, ngd)
+wintor.p <- projectRaster(winTor, mat.cropped) 
+writeRaster(wintor.p, filename = "data/NA", format = "GTiff", bylayer = T, suffix = "names", overwrite = T)
+writeRaster(mat.cropped, filename = "data/NA_mat.tif", format = "GTiff", overwrite = T)
+writeRaster(northing, filename = "data/NA_northing.tif", format = "GTiff", overwrite = T)
+
+
+## Make a new one because fuck it
+frost <- raster("data/NA_nFrostyDays.tif")
+dd0 <- raster(file.path(NA.Clim, "DD_0.asc"))
+proj4string(dd0) <- lcc.proj
+dd0.proj <- projectRaster(dd0, frost )
+dd0.days <- calc(dd0.proj,  fun = function(x) 365-(x/10000)*365)
+names(dd0.days) <- "nDaysFreeze"
+writeRaster(dd0.days, filename = "data/NA_nDaysFreeze.tif", format = "GTiff", overwrite = T)
+frost.freeze <- stack(frost, dd0.days)
+
+frostFreeze <- calc(frost.freeze,
+                    function(x) x[[2]]+ .5*(x[[1]] - x[[2]]))
+names(frostFreeze) <- "NA_frostFreeze"
+writeRaster(frostFreeze, "data/NA_frostFreeze.tif", format = "GTiff")
---
+++ R/prelimScript.R
@@ -0,0 +1,146 @@
+## Preliminary modeling for winTor
+
+library(raster);library(tidyverse)
+library(lubridate)
+
+#link to external
+win.link <- "D://Dropbox/winTor_aux"
+
+dat <- read.csv("data/durationData.csv") #read data
+dat$ID <- 1:nrow(dat)
+
+dat$Start <- as.Date(dat$Start, "%d-%h") # Add time junk
+dat$End<- as.Date(dat$End,"%d-%h")
+
+for(i in 1:nrow(dat)){ # work around for duration 
+  ifelse(is.na(dat$Duration[[i]]), 
+         dat$Duration[[i]] <- dat$Start[[i]]- dat$End[[i]],
+         dat$Duration[[i]] <- dat$Duration[[i]])  
+}
+
+#Convert to spatial object
+coordinates(dat) <- ~ lat + long
+
+#load all the rasters
+env <- stack(file.path("data/", list.files("data/", pattern = "*.tif")))
+#extract estimations for locations
+env.dat <- raster::extract(env, dat, cellnumber = T, df = T)
+
+env.df <- left_join(as.data.frame(dat), env.dat, by = "ID")
+
+library(AICcmodavg)
+## Single Variables
+d1.frost <- lm(formula = Duration ~ NA_nFrostyDays, data = env.df)
+summary(d1.frost)
+
+d1.grow <- lm(formula = Duration ~ NA_nonGrowingDays, data = env.df)
+summary(d1.grow)
+
+d1.north <- lm(formula = Duration ~ NA_northing, data = env.df)
+summary(d1.north)
+
+d1.static <- lm(formula = Duration ~ NA_staticWinter, data = env.df)
+summary(d1.static)
+
+d1.FF <- lm(formula = Duration ~ NA_frostFreeze, data = env.df)
+summary(d1.FF)
+
+d1.freeze <- lm(formula = Duration ~ NA_nDaysFreeze, data = env.df)
+summary(d1.freeze)
+
+#aic Table
+d1.res <- aictab(list(d1.frost, d1.grow, d1.north, d1.static, d1.FF, d1.freeze),
+       modnames = c("frost", "grow", "north", "static", "frost.freeze", "freeze"))
+write.csv(d1.res,file =  file.path(win.link, "Results", 'd1AICtable.csv'), row.names = F)
+
+
+## Two Variables
+d2.north.frost <- lm(formula = Duration ~ NA_nFrostyDays + NA_northing, data = env.df)
+summary(d2.north.frost)
+
+d2.north.grow <- lm(formula = Duration ~ NA_nonGrowingDays + NA_northing, data = env.df)
+summary(d2.north.grow)
+
+d2.north.static <- lm(formula = Duration ~  NA_staticWinter + NA_northing, data = env.df)
+summary(d2.north.static)
+
+d2.north.dem <- lm(formula = Duration ~  NA_dem + NA_northing, data = env.df)
+summary(d2.north.dem)
+
+d2.north.ff <- lm(formula = Duration ~  NA_frostFreeze + NA_northing, data = env.df)
+summary(d2.north.ff)
+
+d2.north.freeze <- lm(formula = Duration ~  NA_nDaysFreeze + NA_northing, data = env.df)
+summary(d2.north.freeze)
+
+#AIC Table
+d2.res <- aictab(list(d2.north.frost, d2.north.grow, d2.north.static, d2.north.dem, d2.north.ff, d2.north.freeze),
+       modnames = c("frost", "grow", "static", "dem", "frost.freeze", "freeze"))
+write.csv(d2.res, file = file.path(win.link, "Results", "d2AICtable.csv"), row.names = F)
+
+## Three Variables
+d3.north.dem.frost <- lm(formula = Duration ~ NA_nFrostyDays + NA_northing + NA_dem, data = env.df)
+summary(d3.north.dem.frost)
+
+d3.north.dem.grow <- lm(formula = Duration ~ NA_nonGrowingDays + NA_northing + NA_dem, data = env.df)
+summary(d3.north.dem.grow)
+
+d3.north.dem.static <- lm(formula = Duration ~  NA_staticWinter + NA_northing + NA_dem, data = env.df)
+summary(d3.north.dem.static)
+
+d3.north.dem.ff <- lm(formula = Duration ~  NA_frostFreeze + NA_northing + NA_dem, data = env.df)
+summary(d3.north.dem.ff)
+
+d3.north.dem.freeze <- lm(formula = Duration ~  NA_nDaysFreeze + NA_northing + NA_dem, data = env.df)
+summary(d3.north.dem.freeze)
+
+aictab(list(d3.north.dem.frost, d3.north.dem.grow,d3.north.dem.static, d3.north.dem.ff, d3.north.dem.freeze),
+       modnames = c("frost", "grow", "static", "frostFreeze", "freeze"))
+
+## Interaction models
+d2i.north.dem <- lm(formula = Duration ~  NA_dem * NA_northing, data = env.df)
+summary(d2i.north.dem)
+
+d3i.north.dem.ff <- lm(formula = Duration ~  NA_frostFreeze + NA_northing * NA_dem, data = env.df)
+summary(d3i.north.dem.ff)
+
+d3i.north.dem.frost <- lm(formula = Duration ~  NA_nFrostyDays + NA_northing * NA_dem, data = env.df)
+summary(d3i.north.dem.frost)
+
+d3i.north.dem.grow <- lm(formula = Duration ~  NA_nonGrowingDays + NA_northing * NA_dem, data = env.df)
+summary(d3i.north.dem.grow)
+
+d3i.north.dem.freeze <- lm(formula = Duration ~  NA_nDaysFreeze + NA_northing * NA_dem, data = env.df)
+summary(d3i.north.dem.freeze)
+
+aictab(list(d2i.north.dem, d3i.north.dem.ff, d3i.north.dem.frost, d3i.north.dem.grow, d3i.north.dem.freeze),
+       modnames = c("dem","frostFreeze", "frost", "grow", "freeze"))
+
+## I reckon that d2i.north.dem is the ticket?
+#this might be how to do it?
+env$NA_northing <- mask(env$NA_northing, env$NA_dem)
+writeRaster(env$NA_northing, "data/NA_northing.tif", format = "GTiff", overwrite = T)
+stk <- stack(env$NA_northing, env$NA_dem)
+north.dem <- calc(stk, function(x){d2i.north.dem$coefficients[[1]] + x[[2]]*d2i.north.dem$coefficients[[2]]
+  + x[[1]]*d2i.north.dem$coefficients[[3]] + x[[1]]*x[[2]]*d2i.north.dem$coefficients[[4]]})
+
+## i don't think that's right...
+stk.df <- as.data.frame(stk) # make to dataframe
+stk.df$cell <- 1:nrow(stk.df)
+df.predict <- predict.lm(d2i.north.dem, newdata = stk.df, interval = "predict", type = "response")
+res.df <- cbind(stk.df, df.predict)
+res.df$fit[res.df$fit > 365] <- 365 #limit max
+res.df$fit[res.df$fit < 0] <- 0 #limit min
+
+
+#lets get this back into a raster
+empty.raster <- env$NA_dem
+empty.raster[] <- NA_real_
+
+empty.raster[res.df$cell] <- res.df$fit
+plot(empty.raster)
+m <- matrix(data = c(cellStats(empty.raster,min),cellStats(empty.raster, max),
+                     0,365,
+                     0,365),
+            nrow = 2)
+doink <- reclassify(empty.raster, m)
