c63571943aaae511d3c5655b11c0cc7397124e8d
Carter Reed Hranac <crh244@gmail.com>
2018-09-11 14:58:34 +1200

    Rework record munge
    Found issues working through winCenter, reworked recordMunge to address

--- R/winCenter.R
+++ R/winCenter.R
@@ -146,7 +146,7 @@ df.est <- df %>%
 ## fix for years...
 year(df.est$est.End) <- df.est$y
 
-## estimate center
+#### estimate center m1 ####
 df.est <- df.est %>%
   mutate( est.Center = est.End - floor(Duration/2),
           est.Dif = est.Center - win.center,
@@ -161,3 +161,243 @@ hist(as.numeric(df.est$est.Dif))
 hist(as.numeric(df.est$fx.Dif))
 
 df.est$win.center
+
+#### Estimate winter center through LM methods ####
+## clean data to use 
+m2.df <- df.est %>%
+  select(id, Start, End, Duration, win.center) %>% #Select columns
+  mutate(n.Start = yday(Start) - 182, ##create columns for numerical date of year
+         n.End = yday(End) + 183, 
+         n.center = abs(yday(win.center) - 182)) ## centered on the other side of the year
+## check : duration should remain the same
+all.equal(m2.df$n.End - m2.df$n.Start, m2.df$Duration) ## i appear to be off by one day...
+## ammended n.End to 183 to fix & re-run
+## still not perfect. there appear to be 10 insances of leap years involved... good enough?
+## yes
+## check to make sure that center is inbetween start and end
+m2.df$n.Start < m2.df$n.center && m2.df$n.center < m2.df$n.End #TRUE
+
+library(raster);library(rgdal)
+
+## add spatial orientation
+wgs84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+coordinates(m2.df) <- ~ long + lat
+proj4string(m2.df)  <- wgs84
+
+## Extract covariates and ammend to df
+## Co-variates
+env.names <- c("NA_dem", "NA_northing", "NA_nFrostyDays",
+               "NA_nonGrowingDays", "NA_nDaysFreeze", "NA_frostFreeze", "NA_OG1k")
+env.stk <- raster::stack(paste0(win.dat,"/",env.names,".tif"))
+
+## extract data from locations
+env.dat <- raster::extract(env.stk, m2.df, cellnumber = T, df = T)
+
+env.df <- as_tibble(left_join(as.data.frame(m2.df), env.dat, by = "ID"))
+
+
+## begin model
+## attempt winter center
+
+## apply rapid lm
+scrapeResults <- function(x){
+  ### Function for creating an easy results table 
+  results <- tidy(x)
+  tidy.table <- results[which(results$term != "(Intercept)"),]
+  bonus.cols <- cbind(intercept.est = results[which(results$term == "(Intercept)"), "estimate"],
+                      intercept.Pval = results[which(results$term == "(Intercept)"), "p.value"],
+                      r.squared = summary(x)$r.squared)
+  res.out <- cbind(tidy.table, bonus.cols)
+  return(res.out)
+}
+
+rapid.lm <- function(x){
+  ###function for lapplying lm
+  f1.lm <- lm(formula = x$mod, data = x$dat)
+  pdf(file = file.path(win.res,paste0(replace(x$mod[length(x$mod)], "+", "_"),".pdf")))
+  par(mfrow = c(3,2))
+  plot(f1.lm, which = 1:6, main = x$mod[[length(x$mod)]], ask = F)
+  dev.off()
+  
+  return(f1.lm)
+}
+
+
+
+
+## formulas
+mod.formulas <- list(n.center ~ NA_dem,
+                     n.center ~ NA_northing,
+                     n.center ~ NA_nFrostyDays,
+                     n.center ~ NA_nonGrowingDays,
+                     n.center ~ NA_nDaysFreeze,
+                     n.center ~ NA_frostFreeze,
+                     n.center ~ NA_OG1k)
+
+## args list
+f1.list <- list()
+for(i in 1:length(mod.formulas)){
+  f1.list[[i]] <- list(mod = mod.formulas[[i]], dat = env.df)
+}
+
+## models
+f1.mod <- lapply(f1.list, rapid.lm)
+
+
+## summaries
+f1.sum <- lapply(f1.mod, summary.lm)
+
+## AIC tab
+library(AICcmodavg)
+f1.res <- aictab(f1.mod, 
+                 modnames = c("dem",
+                              "northing",
+                              "frost",
+                              "growing",
+                              "freeze",
+                              "frostFreeze",
+                              "OG"))
+##write
+write.csv(f1.res,file =  file.path(win.res, 'f1AICtable.csv'), row.names = F)
+
+## results table
+f1.scrape <- lapply(f1.mod, scrapeResults)
+f1.scrape.df <- do.call(rbind, f1.scrape)
+
+write.csv(f1.scrape.df, file = file.path(win.res, 'f1Results.csv'), row.names = F)
+
+##Create prediction rasters
+
+f1Pred.rasters <- lapply(f1.mod, FUN = raster::predict, object = env.stk)
+pred.stk <- do.call(stack, f1Pred.rasters)
+names(pred.stk) <- c("dem",
+                     "northing",
+                     "frost",
+                     "growing",
+                     "freeze",
+                     "frostFreeze",
+                     "OG")
+writeRaster(pred.stk, filename = file.path(win.res, "f1Pred"),
+            format = "GTiff", bylayer = T, suffix = "names", overwrite = T)
+
+#### Level 2 models ####
+
+## formulas
+mod.formulas2 <- list(n.center ~ NA_northing + NA_dem,
+                      n.center ~ NA_northing + NA_nFrostyDays,
+                      n.center ~ NA_northing + NA_nonGrowingDays,
+                      n.center ~ NA_northing + NA_nDaysFreeze,
+                      n.center ~ NA_northing + NA_frostFreeze,
+                      n.center ~ NA_northing + NA_OG1k)
+
+## args list
+f2.list <- list()
+for(i in 1:length(mod.formulas2)){
+  f2.list[[i]] <- list(mod = mod.formulas2[[i]], dat = env.df)
+}
+
+## models
+f2.mod <- lapply(f2.list, rapid.lm)
+
+## summaries
+f2.sum <- lapply(f2.mod, summary.lm)
+
+## AIC table
+f2.res <- aictab(f2.mod, 
+                 modnames = c("dem",
+                              "frost",
+                              "growing",
+                              "freeze",
+                              "frostFreeze",
+                              "OG"))
+#write
+write.csv(f2.res,file =  file.path(win.res, 'f2AICtable.csv'), row.names = F)
+
+## results table
+f2.scrape <- lapply(f2.mod, scrapeResults)
+f2.scrape.df <- do.call(rbind, f2.scrape)
+
+write.csv(f2.scrape.df, file = file.path(win.res, 'f2Results.csv'), row.names = F)
+
+## Create prediction rasters
+
+f2Pred.rasters <- lapply(f2.mod, FUN = raster::predict, object = env.stk)
+pred.stk2 <- do.call(stack, f2Pred.rasters)
+names(pred.stk2) <- c("dem",
+                      "frost",
+                      "growing",
+                      "freeze",
+                      "frostFreeze",
+                      "OG")
+writeRaster(pred.stk2, filename = file.path(win.res, "f2Pred"),
+            format = "GTiff", bylayer = T, suffix = "names", overwrite = T)
+
+#### Level 3 models ####
+
+## formulas
+mod.formulas3 <- list(n.center ~ NA_northing + NA_nFrostyDays + NA_dem,
+                      n.center ~ NA_northing + NA_nonGrowingDays + NA_dem,
+                      n.center ~ NA_northing + NA_nDaysFreeze + NA_dem,
+                      n.center ~ NA_northing + NA_frostFreeze + NA_dem,
+                      n.center ~ NA_northing + NA_OG1k + NA_dem)
+
+f3.list <- list()
+for(i in 1:length(mod.formulas3)){
+  f3.list[[i]] <- list(mod = mod.formulas3[[i]], dat = env.df)
+}
+
+## models
+f3.mod <- lapply(f3.list, rapid.lm)
+
+## Summaries
+f3.sum <- lapply(f3.mod, summary.lm)
+
+## AIC table
+f3.res <- aictab(f3.mod, 
+                 modnames = c("frost",
+                              "growing",
+                              "freeze",
+                              "frostFreeze",
+                              "OG"))
+##write
+write.csv(f3.res,file =  file.path(win.res, 'f3AICtable.csv'), row.names = F)
+
+## results table
+f3.scrape <- lapply(f3.mod, scrapeResults)
+f3.scrape.df <- do.call(rbind, f3.scrape)
+
+write.csv(f3.scrape.df, file = file.path(win.res, 'f3Results.csv'), row.names = F)
+
+## Create prediction rasters
+
+f3Pred.rasters <- lapply(f3.mod, FUN = raster::predict, object = env.stk)
+pred.stk3 <- do.call(stack, f3Pred.rasters)
+names(pred.stk3) <- c("frost",
+                      "growing",
+                      "freeze",
+                      "frostFreeze",
+                      "OG")
+writeRaster(pred.stk2, filename = file.path(win.res, "f3Pred"),
+            format = "GTiff", bylayer = T, suffix = "names", overwrite = T)
+
+#### making a bunch of pretty figures ####
+major.table <- aictab(c(f1.mod, f2.mod, f3.mod), 
+                      modnames = c("dem",
+                                   "northing",
+                                   "frost",
+                                   "growing",
+                                   "freeze",
+                                   "frostFreeze",
+                                   "OG",
+                                   "north + dem",
+                                   "north + frost",
+                                   "north + growing",
+                                   "north + freeze",
+                                   "north + frostFreeze",
+                                   "north + OG",
+                                   "north + dem + frost",
+                                   "north + dem + growing",
+                                   "north + dem + freeze",
+                                   "north + dem + frostFreeze",
+                                   "north + dem + OG"))
+write.csv(major.table, file =  file.path(win.res, 'allModelAICtable.csv'), row.names = F)
--- munge/recordMunge.R
+++ munge/recordMunge.R
@@ -1,30 +1,18 @@
-#### Recorder Munge ####
-library(tidyverse);library(skimr); library(rgdal)
+#### Record munge Re-work
+library(tidyselect); library(skimr); library(rgdal); library(raster); library(lubridate)
 
-## Dirty data frame
-dirt <- read.csv("data/WinterDurationP1.csv", stringsAsFactors = F)
-dirt <- tbl_df(dirt)
-## add ID collumn
-dirt$id <- paste0("id_", 1:nrow(dirt))
-
-## modify ND (No data) into NA
-dirt.NA <- dirt %>%
-  mutate_all(function(x)gsub(pattern = "Insufficient Number Recordings", replacement = NA, x)) %>%
-  mutate_all(function(x)gsub(pattern = "ND", replacement = NA, x))
-
-## Split off the location info and work on it
-
+#### Functions #####
 geoManager <- function(x){
   ### Function to clean up, convert and standarize all georeferencing in the data
   
   ## Subsection off the geo components
   geo.dat <- x %>%
-    select(id, siteName, zone, northing, easting, lat, long, elevation)
+    dplyr::select(ID, siteName, zone, northing, easting, lat, long, elevation)
   
   ## Fix all the Long values
   ll.dat <- geo.dat %>%
     dplyr::filter(long != "")
-  x$long[match(ll.dat$id, x$id)] <- -1 * as.numeric(substr(ll.dat$long, 3, nchar(ll.dat$long)))
+  x$long[match(ll.dat$ID, x$ID)] <- -1 * as.numeric(substr(ll.dat$long, 3, nchar(ll.dat$long)))
   
   
   ## Work with UTM
@@ -56,70 +44,13 @@ geoManager <- function(x){
   }
   utm.df <- do.call(rbind, utm.list)
   
-  x$long[match(utm.df$id, x$id)] <- utm.df$long 
-  x$lat[match(utm.df$id, x$id)] <- utm.df$lat 
+  x$long[match(utm.df$ID, x$ID)] <- utm.df$long 
+  x$lat[match(utm.df$ID, x$ID)] <- utm.df$lat 
   x$long <- as.numeric(x$long)
   return(x)
 }
 
-## process
-dat.geo <- geoManager(x = dirt.NA)
-
-## Which don't have any info on spatial
-v<- dat.geo %>%
-  dplyr::filter(is.na(long)) 
-
-dat.pts <- dat.geo %>%
-  filter(!is.na(long))
-
-## Subset to the useful catagories for the analysis
-dat.sub <- dat.pts %>%
-  select(id, lat, long, nWinterFlights, LDH, lastFall,
-         firstSpring, nMaxEmerging, nMaxSpring, LDE, elevation)
-
-## set up dates
-library(lubridate)
-dat.sub$LDH <-  as.Date(dat.sub$LDH, "%d/%m/%Y") 
-dat.sub$LDE <- as.Date(dat.sub$LDE, "%d/%m/%Y")
-dat.sub$firstSpring <- as.Date(dat.sub$firstSpring, "%d/%m/%Y")
-dat.sub$lastFall <- as.Date(dat.sub$lastFall, "%d/%m/%Y")
-
-## duration of witner
-dat.sub$winter.duration <- as.numeric(dat.sub$LDE - dat.sub$LDH)
-hist(dat.sub$winter.duration)
-
-#   ## 2 entries are negative when they should be postitive. 
-# dat.sub[which(dat.sub$winter.duration < 0),] # 29 & 76 ## Changed in source file
-
-#   ## Make sure years are around the correct way
-# dat.sub[which(year(dat.sub$LDE) - year(dat.sub$LDH) < 0),]
-
-
-## differences between the firt of spring and LDE
-dat.sub$spring.diff <- as.numeric(dat.sub$firstSpring - dat.sub$LDE)
-hist(dat.sub$spring.diff)
-
-#   ## Adressing the clear outliers
-# dat.sub[which(abs(dat.sub$spring.diff)>100), ] #144 ## Corrected in source
-
-## difference betweeen last of falll and LDH
-dat.sub$fall.diff <- as.numeric(dat.sub$LDH - dat.sub$lastFall)
-hist(dat.sub$fall.diff)
-
-#   ##addressing outlyiers again
-# dat.sub[which(abs(dat.sub$fall.diff)>100),] ## 29 and 76 ## corrected in source
-
-## writeout
-write.csv(dat.sub, "data/winDurationClean.csv", row.names = F)
-
-
-
-#### Combine with literature data ####
-library(raster);library(tidyverse)
-library(lubridate); library(broom)
-library(AICcmodavg); library(maptools)
-
-## Extra Paths
+#### paths ####
 if (!exists('base.path')) {
   if(.Platform$"OS.type" == "windows"){
     base.path = file.path("D:", "Dropbox", "wintor_aux")
@@ -131,42 +62,72 @@ if (!exists('base.path')) {
 win.dat <- file.path(base.path, "data")
 win.res <- file.path(base.path, "Results")
 
+#### Recorder data ####
+rec.dirty <- tbl_df(read.csv("data/WinterDurationP1.csv", stringsAsFactors = F))
+## add ID col
+rec.dirty$ID <- paste0("ID_", 1:nrow(rec.dirty))
+## modifiy no data entries 
+rec.NA <- rec.dirty %>%
+  mutate_all(function(x)gsub(pattern = "Insufficient Number Recordings", replacement = NA, x)) %>%
+  mutate_all(function(x)gsub(pattern = "ND", replacement = NA, x))
+## clean up spatial issues 
+rec.pts <- geoManager(x = rec.NA)
+## remove those without spatial locations
+rec.dpts <- rec.pts %>%
+  dplyr::filter(!is.na(long))
+## clean the excess columns out 
+rec.sub <- rec.dpts %>%
+  dplyr::select(ID, lat, long, nWinterFlights, LDH, lastFall,
+                firstSpring, nMaxEmerging, nMaxSpring, LDE, elevation)
+## set up dates
+rec.sub$LDH <-  as.Date(rec.sub$LDH, "%d/%m/%Y") 
+rec.sub$LDE <- as.Date(rec.sub$LDE, "%d/%m/%Y")
+rec.sub$firstSpring <- as.Date(rec.sub$firstSpring, "%d/%m/%Y")
+rec.sub$lastFall <- as.Date(rec.sub$lastFall, "%d/%m/%Y")
+rec.sub$winter.duration <- as.numeric(rec.sub$LDE - rec.sub$LDH)
+hist(rec.sub$winter.duration)
+skim(rec.sub)
 
-## literature data
+## final clean
+##remove those entries that don't have useable duration data
+rec.clean <- rec.sub %>%
+  dplyr::filter(!is.na(winter.duration)) %>%
+  dplyr::select(ID, lat, long, LDH, LDE, winter.duration)
+colnames(rec.clean)[4:5] <- c("Start", "End") # rename to conform
+
+#### literature data ####
 lit.dat <- as_tibble(read.csv("data/durationData.csv"))
-lit.dat$id <- paste0("L", 1:nrow(lit.dat)) ## add id
+lit.dat$ID <- paste0("L", 1:nrow(lit.dat)) ## add ID
+colnames(lit.dat)[5] <- "winter.duration"
 
 ## determine duration 
 lit.dat$Start <- as.Date(lit.dat$Start, '%d-%h')
 lit.dat$End <- as.Date(lit.dat$End, '%d-%h')
+## add year for end
+year(lit.dat$End) <- 2019
 
 
-for(i in 1:nrow(lit.dat)){ # Create duration since there's no years in the df
-  ifelse(is.na(lit.dat$Duration[[i]]), 
-         lit.dat$Duration[[i]] <- 365 - (lit.dat$Start[[i]]- lit.dat$End[[i]]),
-         lit.dat$Duration[[i]] <- lit.dat$Duration[[i]])  
+for(i in 1:nrow(lit.dat)){ # Create duration since there's one already in there
+  ifelse(is.na(lit.dat$winter.duration[[i]]), 
+         lit.dat$winter.duration[[i]] <- as.numeric(lit.dat$End[[i]]- lit.dat$Start[[i]]),
+         lit.dat$winter.duration[[i]] <- lit.dat$winter.duration[[i]])  
 }
+## final clean
+lit.clean <- lit.dat %>%
+  dplyr::select(ID, lat, long, Start, End, winter.duration)
 
-lit.sub <- lit.dat %>%
-  dplyr::select(id, long, lat, Duration) %>%
-  rename(winter.duration = Duration)
-
-
-## recorder data
-rec.dat <- as_tibble(read.csv("data/winDurationClean.csv"))
-
-rec.sub <- rec.dat %>%
-  dplyr::select(id, long, lat, winter.duration) %>%
-  filter(!is.na(winter.duration))
-
-## join 
-dat <- bind_rows(lit.sub, rec.sub)
-dat$ID <- 1:nrow(dat)
+#### Complete data frame ####
+dat.comp <- rbind(lit.clean, rec.clean)
+# lat isn't numeric for some reason
+dat.comp$lat <- as.numeric(dat.comp$lat)
+#create id col for binding with env 
+dat.comp$env_ID <- 1:nrow(dat.comp)
+#### spatial co-varriates ####
 
 ## add spatial orientation
 wgs84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
-coordinates(dat) <- ~ long + lat
-proj4string(dat)  <- wgs84
+coordinates(dat.comp) <- ~ long + lat
+proj4string(dat.comp)  <- wgs84
 
 ## Co-variates
 env.names <- c("NA_dem", "NA_northing", "NA_nFrostyDays",
@@ -174,9 +135,9 @@ env.names <- c("NA_dem", "NA_northing", "NA_nFrostyDays",
 env.stk <- raster::subset(stack(list.files(win.dat, pattern = "NA_*", full.names = T)), env.names)
 
 ## extract data from locations
-env.dat <- raster::extract(env.stk, dat, cellnumber = T, df = T)
-
-env.df <- as_tibble(left_join(as.data.frame(dat), env.dat, by = "ID"))
+env.dat <- raster::extract(env.stk, dat.comp, cellnumber = T, df = T)
+colnames(env.dat)[1] <- "env_ID"
+env.df <- as_tibble(left_join(as.data.frame(dat.comp), env.dat))
 
 ## write out complete dataframe to 
-write.csv(env.df, file.path("data/", "modelingDataFrame.csv"), row.names = F)
+write.csv(env.df[,-7], file.path("data/", "modelingDataFrame.csv"), row.names = F)
