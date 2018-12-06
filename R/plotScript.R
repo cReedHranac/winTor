61261705491ea17bc64146356b8d60fe6b8c73ea
Carter Reed Hranac <crh244@gmail.com>
2018-12-06 14:07:15 +1300

    More plots
    plots for all of the various items

--- R/plotScript.R
+++ R/plotScript.R
@@ -169,7 +169,7 @@ mass.plot <- function(x, c.string, res.agg = 25, dist.map = NULL,
 (massMean.plot <- mass.plot(x = massmean,
                             c.string = massColors(5)))
 
-fatMean <- calc(massmean, fun = function(x){-2.84 + 0.59*x})
+fatMean <- calc(massmean, fun = function(x){-2.84 + 0.593*x})
 fat.plot <- function(x, c.string, res.agg = 25, dist.map = NULL,
                       save.name = NULL,  device.out = NULL,  ...){
   ## Create DataFrame (aggragation is mainly for the dev period)
@@ -245,8 +245,163 @@ fatColors <- colorRampPalette(c("#fff7bc","#fec44f", "#d95f0e"))
 
 #### Survival mapping ####
 
+
+## Static conditions 
 fatReq984.null <- raster(file.path(win.res, "MYLU_fatRequired_98_4_fat.null.tif"))
 fatReq984.inf <- raster(file.path(win.res, "MYLU_fatRequired_98_4_fat.inf.tif"))
 
-survStatic.null <- fatReq984.null - fatMean
-plot(survStatic.null)
+survStatic.null <- fatMean - fatReq984.null
+survStatic.inf <- fatMean - fatReq984.inf
+survColorsPOS <- colorRampPalette(c( "#ffffff", "#5e3c99","#b2abd2")) #"#fdb863", "#e66101"
+Surv.plotPOS <- function(x, c.string, res.agg = 25, dist.map = NULL,
+                     save.name = NULL,  device.out = NULL,  ...){
+  ## Create DataFrame (aggragation is mainly for the dev period)
+  if(!is.null(res.agg)){ #aggratetion bits
+    x.ag <- raster::aggregate(x, res.agg)
+  }
+  else{
+    x.ag <- x}
+  ## Crop and mask to distribution
+  if(!is.null(dist.map)){
+    x.ag <- mask(crop(x.ag, dist.map), dist.map)
+  }
+  
+  ## Convert to df
+  x.pts <- rasterToPoints(x.ag) #to points
+  x.df <- data.frame(x.pts)
+  colnames(x.df) <- c("long", "lat", "winter")
+  
+  g.win <- ggplot(data = x.df, aes(x = long, y = lat, z = winter)) +
+    coord_fixed(xlim = extent(x)[1:2], ylim = extent(x)[3:4]) +
+    #border lines
+    borders("world",
+            xlim=extent(x)[1:2],ylim=extent(x)[3:4],
+            colour = "grey20",
+            fill = "grey80")+
+    #Raster fill
+    geom_raster(aes(fill = winter),  interpolate = T) +
+    #oooohhhhh pretty colors
+    scale_fill_gradientn("Predicted\nBody\nFat\nRemaining (g)",
+                         colors = c.string,
+                         limits=  c(floor(minValue(x.ag)),
+                                    ceiling(maxValue(x.ag)))) +
+    
+    #general malarkey
+    scale_x_continuous(expand = c(0,0))+
+    scale_y_continuous(expand = c(0,0))+
+    theme(plot.title = element_text(hjust = .05))+
+    #ggtitle("Predicted Body Mass") + 
+    theme_bw()
+  
+  ##Distribution map flag
+  if(!is.null(dist.map)){
+    g.win <- g.win +
+      geom_polygon(data = fortify(dist.map),
+                   aes(long,lat, group = group),
+                   colour = "black",
+                   fill = NA,
+                   inherit.aes = F) 
+  }
+  
+  if(!is.null(save.name)){
+    if(device.out == "pdf"){
+      dev.ext <- cairo_pdf
+    } else if (device.out =="eps"){
+      dev.ext <- cairo_ps
+    } else {
+      dev.ext <- device.out
+    }
+    ex <- as.vector(extent(x))
+    aspect.ratio <- (ex[[2]] - ex[[1]])/(ex[[4]] - ex[[3]])
+    ggsave(filename = file.path("fig", pasteo(save.name,".", device.out)),
+           g.win, 
+           width = 9, height = 9/aspect.ratio, unit = "in",
+           dpi = 300,
+           device = dev.ext,
+           ...)}
+  
+  return(g.win)
+}
+
+(staticNull.plot <- Surv.plotPOS(survStatic.null, survColorsPOS(5)))
+(staticInf.plot <- Surv.plotPOS(survStatic.inf, survColorsPOS(5)))
+
+
+## Surface conditions
+fatReq.null <- raster(file.path(win.res, "MYLU_fatRequired_fat.null.tif"))
+fatReq.inf <- raster(file.path(win.res, "MYLU_fatRequired_fat.inf.tif"))
+
+surfaceSurv.null <- fatMean - fatReq.null
+surfaceSurv.inf <- fatMean - fatReq.inf
+survColors <- colorRampPalette(c("#fdb863", "#e66101", "#ffffff", "#5e3c99","#b2abd2")) #
+Surv.plot <- function(x, c.string, res.agg = 25, dist.map = NULL,
+                         save.name = NULL,  device.out = NULL,  ...){
+  ## Create DataFrame (aggragation is mainly for the dev period)
+  if(!is.null(res.agg)){ #aggratetion bits
+    x.ag <- raster::aggregate(x, res.agg)
+  }
+  else{
+    x.ag <- x}
+  ## Crop and mask to distribution
+  if(!is.null(dist.map)){
+    x.ag <- mask(crop(x.ag, dist.map), dist.map)
+  }
+  
+  ## Convert to df
+  x.pts <- rasterToPoints(x.ag) #to points
+  x.df <- data.frame(x.pts)
+  colnames(x.df) <- c("long", "lat", "winter")
+  
+  g.win <- ggplot(data = x.df, aes(x = long, y = lat, z = winter)) +
+    coord_fixed(xlim = extent(x)[1:2], ylim = extent(x)[3:4]) +
+    #border lines
+    borders("world",
+            xlim=extent(x)[1:2],ylim=extent(x)[3:4],
+            colour = "grey20",
+            fill = "grey80")+
+    #Raster fill
+    geom_raster(aes(fill = winter),  interpolate = T) +
+    #oooohhhhh pretty colors
+    scale_fill_gradientn("Predicted\nBody\nFat\nRemaining (g)",
+                         colors = c.string,
+                         limits=  c(floor(minValue(x.ag)),
+                                    ceiling(maxValue(x.ag)))) +
+    
+    #general malarkey
+    scale_x_continuous(expand = c(0,0))+
+    scale_y_continuous(expand = c(0,0))+
+    theme(plot.title = element_text(hjust = .05))+
+    #ggtitle("Predicted Body Mass") + 
+    theme_bw()
+  
+  ##Distribution map flag
+  if(!is.null(dist.map)){
+    g.win <- g.win +
+      geom_polygon(data = fortify(dist.map),
+                   aes(long,lat, group = group),
+                   colour = "black",
+                   fill = NA,
+                   inherit.aes = F) 
+  }
+  
+  if(!is.null(save.name)){
+    if(device.out == "pdf"){
+      dev.ext <- cairo_pdf
+    } else if (device.out =="eps"){
+      dev.ext <- cairo_ps
+    } else {
+      dev.ext <- device.out
+    }
+    ex <- as.vector(extent(x))
+    aspect.ratio <- (ex[[2]] - ex[[1]])/(ex[[4]] - ex[[3]])
+    ggsave(filename = file.path("fig", pasteo(save.name,".", device.out)),
+           g.win, 
+           width = 9, height = 9/aspect.ratio, unit = "in",
+           dpi = 300,
+           device = dev.ext,
+           ...)}
+  
+  return(g.win)
+}
+(surfaceNull.plot <- Surv.plot(surfaceSurv.null, survColors(5)))
+(surfaceInf.plot <- Surv.plot(surfaceSurv.inf, survColors(5)))
